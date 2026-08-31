3f3dee839593a3e5d3d3d02e3312a1d30dbe9b10
Alex <alex@sportme.ro>
2026-08-31 10:09:14 +0300

    Hide AI chat from public site

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ReactNode } from "react";
-import { AiChatWidget } from "./components/AiChatWidget";
 import { I18nProvider } from "./app/i18n";
 import { CookieConsent } from "./components/CookieConsent";
 
@@ -9,7 +8,6 @@ export function Providers({ children }: { children: ReactNode }) {
   return (
     <I18nProvider>
       {children}
-      <AiChatWidget />
       <CookieConsent />
     </I18nProvider>
   );
